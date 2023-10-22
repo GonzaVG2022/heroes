a53387f6fdb82e587a8158f9389a52461151ae5d
Flavio Gonzalo Villalba Gargantini <gonzagargantini20@gmail.com>
2023-10-21 23:38:35 -0300

    Fin Heroes

--- src/heroes/routes/HeroesRoutes.jsx
+++ src/heroes/routes/HeroesRoutes.jsx
@@ -1,20 +1,26 @@
-import { Navigate, Route, Routes } from "react-router-dom"
-import { Navbar } from "../../ui"
-import { DcPage, HeroPage, MarvelPage, SearchPage } from "../pages"
-
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { Navbar } from '../../ui';
+import { DcPage, HeroPage, MarvelPage, SearchPage } from '../pages';
 
 export const HeroesRoutes = () => {
   return (
     <>
-        <Navbar/>
-        <Routes>
-          <Route path="marvel" element={ <MarvelPage/> }/> {/* 👈 Renders at /app/ */}
-          <Route path="dc" element={<DcPage/>}/>
-          <Route path="/" element={<Navigate to={'/marvel'}/>}/>
-          <Route path="/search" element={<SearchPage to={'/marvel'}/>}/>
-          <Route path="/hero/:heroid" element={<HeroPage to={'/marvel'}/>}/>
+        <Navbar />
 
-        </Routes> 
+        <div className="container">
+            <Routes>
+                <Route path="marvel" element={<MarvelPage />} />
+                <Route path="dc" element={<DcPage />} />
+                
+                <Route path="search" element={<SearchPage />} />
+                <Route path="hero/:id" element={<HeroPage />} />
+                                
+
+                <Route path="/" element={<Navigate to="/marvel" />} />
+
+            </Routes>
+        </div>
+
 
     </>
   )
